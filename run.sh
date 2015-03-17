9e7a9483adac997ede850f74f6c1473cb281f79e
Massimo Nicosia <m.nicosia@gmail.com>
2015-03-17 18:27:52 +0100

    Run script

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-source classpath.sh
-
-java -Djava.util.logging.config.file="resources/logging.properties" \
--Xss128m \
--cp ${CLASSPATH} qa.qcri.qf.pipeline.trec.TrecPipelineRunner \
--argumentsFilePath arguments/trec-en-pipeline-arguments.txt
+java -cp target/dependency/*:target/qa.qcri.qf.pipeline-0.0.1-SNAPSHOT.jar \
+ -Djava.util.logging.config.file="resources/logging.properties" \
+ -Xss128m \
+ qa.qcri.qf.pipeline.trec.TrecPipelineRunner \
+ -argumentsFilePath arguments/trec-en-pipeline-arguments.txt
