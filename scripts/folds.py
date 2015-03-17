c34328b3f043226bf69ec60d0e4fff9b8f3c1da0
Massimo Nicosia <m.nicosia@gmail.com>
2015-03-17 18:27:14 +0100

    No prints

--- scripts/folds.py
+++ scripts/folds.py
@@ -95,11 +95,12 @@ def main():
 		
 		for fold_id in xrange(n_folds):
 			if fold_id == qid_fold:
-				print "[INFO]: Not writing this example in fold:", fold_id, "[", qid_fold, "]"
+				pass
+				#print "[INFO]: Not writing this example in fold:", fold_id, "[", qid_fold, "]"
 			else:	
 				folds_f[fold_id].write(example + "\n")
 				folds_res_f[fold_id].write(res + "\n")
-				print "[INFO]: Writing training examples related to question", example_qid, "in fold", fold_id, "[", qid_fold, "]"
+				#print "[INFO]: Writing training examples related to question", example_qid, "in fold", fold_id, "[", qid_fold, "]"
 			
 	for fold_id in folds_f:
 		folds_f[fold_id].close()
@@ -125,16 +126,16 @@ def main():
 			if fold_id == qid_fold: 
 				folds_f[fold_id].write(example + "\n")
 				folds_res_f[fold_id].write(res + "\n")
-				print "[INFO]: Writing test examples related to question", example_qid, "in fold", fold_id, "[", qid_fold, "]"
+				#print "[INFO]: Writing test examples related to question", example_qid, "in fold", fold_id, "[", qid_fold, "]"
 			else:
-				print "[INFO]: Not writing this example in fold:", fold_id, "[", qid_fold, "]"
+				pass
+				#print "[INFO]: Not writing this example in fold:", fold_id, "[", qid_fold, "]"
 			
 	for fold_id in folds_f:
 		folds_f[fold_id].close()
 		folds_res_f[fold_id].close()
 		
 	print_output_stats(dir, n_folds)
-	print_eacl_folds_stats(n_folds)
 		
 def print_output_stats(dir, n_folds):
 	print "#### SCRIPT OUTPUT"
@@ -151,69 +152,7 @@ def print_output_stats(dir, n_folds):
 		print "\tNumber of questions in test fold:", len(test_qids)
 		print "\tNumber of questions in common:", len(train_qids.intersection(test_qids))
 		print ""
-	
 		
-def print_eacl_folds_stats(n_folds):
-	print "#### EACL DATA"
-	
-	directory = "data/trec-en/chvfq-2013-10-14/"
-	
-	for fold_id in xrange(n_folds):
-		train_f_res = directory + "fold" + str(fold_id) + "/svm.train.res"
-		test_f_res = directory + "fold" + str(fold_id) + "/svm.relevancy"
-		train_qids = set([line.strip().split(" ")[0] for line in open(train_f_res, "r")])
-		test_qids = set([line.strip().split(" ")[0] for line in open(test_f_res, "r")])
-		
-		print "Fold id:", fold_id
-		print "\tNumber of questions in train fold:", len(train_qids)
-		print "\tNumber of questions in test fold:", len(test_qids)
-		print "\tNumber of questions in common:", len(train_qids.intersection(test_qids))
-		print ""
-		
-def post_check(n_folds):
-	for fold_id in xrange(n_folds):
-		eacl_dir = "data/trec-en/chvfq-2013-10-14/"
-		eacl_train_f_res = eacl_dir + "fold" + str(fold_id) + "/svm.train.res"
-		eacl_train_qids = set([line.strip().split(" ")[0] for line in open(eacl_train_f_res, "r")])
-		eacl_test_f_res = eacl_dir + "fold" + str(fold_id) + "/svm.relevancy"
-		eacl_test_qids = set([line.strip().split(" ")[0] for line in open(eacl_test_f_res, "r")])
-		
-		fold_dir = "data/trec-en/"
-		train_f_res = fold_dir + "folds/fold-" + str(fold_id) + "/svm.train.res"
-		train_qids = set([line.strip().split(" ")[0] for line in open(train_f_res, "r")])
-		test_f_res = fold_dir + "folds/fold-" + str(fold_id) + "/svm.test.res"
-		test_qids = set([line.strip().split(" ")[0] for line in open(test_f_res, "r")])
-		
-		print "#questions in EACL and trec-en train folds", fold_id, ":", len(eacl_train_qids), len(train_qids) 
-		print "#questions in EACL and trec-en test folds", fold_id, ":", len(eacl_test_qids), len(test_qids) 
-		
-		print "EACL and trec-en train fold", fold_id, "mismatch:", len(eacl_train_qids.difference(train_qids)) 
-		print "EACL and trec-en test fold", fold_id, "mismatch:", len(eacl_test_qids.difference(test_qids)) 
-	
-def load_eacl_folds_data(n_folds):
-	directory = "data/trec-en/chvfq-2013-10-14/"
-	
-	qid_2_fold = {}
-	
-	for fold_id in xrange(n_folds):
-		test_f_res = directory + "fold" + str(fold_id) + "/svm.relevancy"
-		
-		qids = set([line.strip().split(" ")[0] for line in open(test_f_res, "r")])
-		qid_2_fold.update({qid : fold_id for qid in qids})
-		
-	# Add missing question #1529, #2167, #1714, #1896 to folds
-	qid_2_fold["1529"] = 0
-	qid_2_fold["2167"] = 1
-	qid_2_fold["1714"] = 2
-	qid_2_fold["1896"] = 3
-	
-	count = Counter(qid_2_fold.values())
-	for elem in count:
-		print "Number of questions in EACL fold", elem, ":", count[elem]
-		
-	return qid_2_fold
-		
-	
 def slice_it(li, cols=2):
 	start = 0
 	for i in xrange(cols):
